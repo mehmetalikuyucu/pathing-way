20d7fd19b0e25077ae0628416154b7b11b04d598
mehmetalikuyucu <mkuyucu443@gmail.com>
2024-07-25 07:35:14 +0300

    feat:init

---
+++ src/main/java/com/pathing_way/pathing_way/PathingWayApplication.java
@@ -0,0 +1,13 @@
+package com.pathing_way.pathing_way;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathingWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathingWayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pathing_way/pathing_way/PathingWayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pathing_way.pathing_way;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathingWayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
